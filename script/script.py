9b92d9260ac441f0b04707c38a5d6b9aadcb10c5
Bram Michielsen <bram-michielsen@becode.xyz>
2023-08-31 07:12:09 +0200

    Solution to the second question

--- script/script.py
+++ script/script.py
@@ -1,14 +1,13 @@
 import sqlite3
 
 # Connect to the SQLite database
-conn = sqlite3.connect("sqlite///../data/vivino.db")
+conn = sqlite3.connect("sqlite:///../data/vivino.db")
 cursor = conn.cursor()
-
 
 # Query to Get Vintage Ratings by Country and Year:
 # This query calculates the total number of ratings for each combination of country, year, and vintage.
 
-query1 = """
+query_vintage_ratings = """
 SELECT
     c.name AS country,
     v.year,
@@ -28,7 +27,7 @@ GROUP BY
 # Query to Calculate Yearly Growth in Ratings:
 # This query calculates the growth rate of ratings from one year to the next for each country.
 
-query2 = """
+query_rating_growth = """
 WITH ratings_by_year AS (
     SELECT
         c.name AS country,
@@ -58,21 +57,67 @@ JOIN
     ratings_by_year r2 ON r1.country = r2.country AND r1.year = r2.year + 1;
 """
 
-# Execute the queries
-cursor.execute(query1)
-results1 = cursor.fetchall()
+# Query to Get Average Wine Rating by Country:
+query_avg_wine_rating = """
+SELECT
+    c.name AS country,
+    AVG(w.ratings_average) AS avg_wine_rating
+FROM
+    countries c
+JOIN
+    regions r ON c.code = r.country_code
+JOIN
+    wines w ON r.id = w.region_id
+GROUP BY
+    c.name;
+"""
 
-cursor.execute(query2)
-results2 = cursor.fetchall()
+# Query to Get Average Vintage Rating by Country:
+query_avg_vintage_rating = """
+SELECT
+    c.name AS country,
+    AVG(v.ratings_average) AS avg_vintage_rating
+FROM
+    countries c
+JOIN
+    regions r ON c.code = r.country_code
+JOIN
+    wines w ON r.id = w.region_id
+JOIN
+    vintages v ON w.id = v.wine_id
+GROUP BY
+    c.name;
+"""
+
+# Execute the queries
+cursor.execute(query_vintage_ratings)
+results_vintage_ratings = cursor.fetchall()
+
+cursor.execute(query_rating_growth)
+results_rating_growth = cursor.fetchall()
+
+cursor.execute(query_avg_wine_rating)
+results_country_avg = cursor.fetchall()
+
+cursor.execute(query_avg_vintage_rating)
+results_vintage_avg = cursor.fetchall()
 
 # Close the database connection
 conn.close()
 
 # Print or process the results as needed
 print("Vintage Ratings by Country and Year:")
-for row in results1:
+for row in results_vintage_ratings:
     print(row)
 
 print("\nYearly Growth in Ratings:")
-for row in results2:
+for row in results_rating_growth:
+    print(row)
+
+print("\nAverage Wine Ratings by Country:")
+for row in results_country_avg:
+    print(row)
+
+print("\nAverage Wine Ratings by Vintage:")
+for row in results_vintage_avg:
     print(row)
